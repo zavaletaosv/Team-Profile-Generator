a633c4c2ee797ee0a24155400bcb994c18c39752
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-07-30 12:16:43 -0500

    Employee.test.js updated: tests created

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,48 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee class", () => {
+  it("can be instantiated", () => {
+    const employee = new Employee();
+    expect(typeof employee).toBe("object");
+    expect(employee instanceof Employee).toBe(true);
+  });
+
+  it("should have a name property that can be set on instanstiation", () => {
+    const employee = new Employee("Osvaldo");
+    expect(employee.name).toBe("Osvaldo");
+  });
+
+  it("should have a id property that can be set on instanstiation", () => {
+    const employee = new Employee("Osvaldo", 1);
+    expect(employee.id).toBe(1);
+  });
+
+  it("should have an email property that can be set on instanstiation", () => {
+    const employee = new Employee("Osvaldo", 1, "osvaldo@testemail.com");
+    expect(employee.email).toBe("osvaldo@testemail.com");
+  });
+
+  it("should return an employee name via getName()", () => {
+    const test = "Osvaldo";
+    const employee = new Employee(test);
+    expect(employee.getName()).toBe(test);
+  });
+
+  it("should return an employee id via getId()", () => {
+    const test = 100;
+    const employee = new Employee("Osvaldo", test);
+    expect(employee.getId()).toBe(test);
+  });
+
+  it("should return an employee email via getEmail()", () => {
+    const test = "osvaldo@testemail.com";
+    const employee = new Employee("Osvaldo", 100, test);
+    expect(employee.getEmail()).toBe(test);
+  });
+
+  it("should return an employee via getRole()", () => {
+    const test = "Employee";
+    const employee = new Employee(test);
+    expect(employee.getRole()).toBe(test);
+  });
+});
