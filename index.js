4ba684d667817681b268cd3ec99187284b7c0261
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-07-30 17:44:35 -0500

    index.js updated: import packages added

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
